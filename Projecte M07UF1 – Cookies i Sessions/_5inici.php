cc8b6089e33bcca7f6dde63823ec2204e91021cc
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2024-10-16 17:33:38 +0200

    añadir 4 preguntas más

--- Projecte M07UF1 – Cookies i Sessions/_5inici.php
+++ Projecte M07UF1 – Cookies i Sessions/_5inici.php
@@ -23,6 +23,21 @@ if (isset($_COOKIE['USUARI']) && isset($_COOKIE['PASSWORD'])) {
     <input type="text" name="usuari" id="usuari" value='<?php echo isset($_COOKIE["USUARI"]) ? $_COOKIE["USUARI"] : "" ?>' /><br /><br />
     <label for="contrasenya">Contrasenya: </label>
     <input type="password" name="contrasenya" id="contrasenya" value='<?php echo isset($_COOKIE["PASSWORD"]) ? $_COOKIE["PASSWORD"] : "" ?>' /><br /><br />
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email"><br><br>
+    <label for="tel">Telefon</label>
+    <input type="tel" name="tel" id="tel"><br><br>
+
+    <label for="fecha">Edad</label>
+    <input type="date" id="fecha" name="fecha" ><br>
+    <p>¿Cuál es tu género?</p>
+    <input type="radio" id="hombre" name="genero" value="hombre" >
+    <label for="hombre">Hombre</label><br>
+    <input type="radio" id="mujer" name="genero" value="mujer">
+    <label for="mujer">Mujer</label><br>
+    <input type="radio" id="otro" name="genero" value="otro">
+    <label for="otro">Otro</label><br><br>
+
     <label for="checkbox"> Guardar cookies</label>
     <input type="checkbox" name="checkbox" id="checkbox" /> <br /><br />
     <input type="submit" name="enviar" value="Accedir" />
